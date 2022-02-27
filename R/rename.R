e938ecdfaa00508d0c807b67f27dcc1420433287
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2022-02-27 12:33:16 +0000

    Clarify reasoning for callback rules

--- R/rename.R
+++ R/rename.R
@@ -14,6 +14,14 @@
 rename_rendered <- function(file, callback) {
   metadata <- rmd_metadata(file)
 
+  # Allowing directory changes would cause confusion if `file` is not in the
+  # current working directory -- would the result be relative to the current
+  # directory or to the original file? Relative to current directory seems
+  # like the "correct" but less useful behaviour. Simpler to disallow it.
+  #
+  # Explicitly specified file extensions could be allowed, and still inherit
+  # the extension if not specified. I don't really see a use for that though,
+  # so we avoid a branch by leaving it out unless a real need somehow arises.
   new_name <- fs::path_file(callback(metadata))
   new_file <- fs::path_ext_set(new_name, fs::path_ext(file))
   new_path <- fs::path(fs::path_dir(file), new_file)
